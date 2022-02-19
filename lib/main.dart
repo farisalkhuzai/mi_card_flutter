9a61c3fda734d13518e1eb23d6b9cc3a4ad16834
farisalkhuzai <alkhuzaifaris@gmail.com>
2022-02-19 20:09:17 +0300

    farisUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.teal[300],
+                backgroundImage: AssetImage('Images/rock.png'),
+              ),
+              Text(
+                  'Faris Alkhuzai',
+                  style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+
+                  ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text('+966 59 811 6735',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: ('Source Sans Pro'),
+                        fontSize: 20.0
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+
+                child: ListTile(
+                  leading:Icon(Icons.email,
+                    color: Colors.teal,),
+                  title: Text('alkhuzaifaris@gmail.com',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'
+                      )),
+
+                ),
+
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
